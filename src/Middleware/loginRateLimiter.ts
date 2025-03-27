758c6885d37beb99a0ea218186d9f364941aab4b
maneesh-settipeta <maneeshsettipeta@gmail.com>
2025-03-27 14:28:32 -0500

    pushing on 3-27-2015

---
+++ src/Middleware/loginRateLimiter.ts
@@ -0,0 +1,35 @@
+import redis from "../redisClient";
+import { Request, Response, NextFunction } from "express";
+
+const loginRateLimiter = async (req: Request, res: Response, next: NextFunction) => {
+    console.log(req.headers);
+    console.log(req.headers.email);
+    const email = req.headers.email;
+    console.log(email);
+
+    const max_request = 2;
+    const key = `login-credentials ${email}`;
+    const max_expire = 60;
+    if (!key) {
+        return res.status(400).json({ message: "Request email is undefined" });
+    }
+
+    try {
+        await redis.incr(key);
+        const attempts = await redis.get(key);
+        console.log(attempts, "count");
+
+        if (attempts && parseInt(attempts) >= max_request) {
+            return res.status(429).json({ message: "Too Many Attempts" });
+        }
+        await redis.expire(key, max_expire);
+        next();
+
+    } catch (error) {
+        console.error(error, "Redis Error");
+        return res.status(500).json({ message: "Server error. Please try again later" });
+    }
+
+}
+
+export default loginRateLimiter;
--- src/routes/Routes.ts
+++ src/routes/Routes.ts
@@ -1,10 +1,10 @@
 import express from "express";
-import { signin, signup, landingPage } from "../controllers/userController";
+import { signin, signup } from "../controllers/userController";
+import loginRateLimiter from '../Middleware/loginRateLimiter';
 
 const router = express.Router();
 
 router.post('/signup', signup);
-router.post('/signin', signin);
-router.get('/landingPage', landingPage);
+router.post('/signin', loginRateLimiter, signin);
 
 export default router;
